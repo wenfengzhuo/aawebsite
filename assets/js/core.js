84347cac1964331c1e31a7036bf4e2b9a814854f
Wenfeng Zhuo <wz2366@columbia.edu>
2017-03-07 11:55:25 -0500

    fix event height issue and add job page

--- assets/js/core.js
+++ assets/js/core.js
@@ -25,13 +25,6 @@ $(document).ready(function(){
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
